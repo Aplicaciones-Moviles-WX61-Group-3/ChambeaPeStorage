6ebdd93e41ed059923ff2e0519fddea879608ac5
steve <stev8855@gmail.com>
2024-05-20 13:12:30 -0500

    feat: update route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -9,6 +9,11 @@ export const createUserRoute = () => {
     const userController = new UserController({ userService });
 
     router.get("/", userController.getAllUsers);
+    router.get("/:id_user", userController.getUserById);
+    router.post("/", userController.createUser);
+    router.delete("/:id_user", userController.deleteUser);
+    router.patch("/:id_user/addImageUrl", userController.addImageUrl);
+    router.patch("/:id_user/removeImageUrl", userController.removeImageUrl);
 
     return router;
 }
