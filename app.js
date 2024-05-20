ac8a204dd232aaf4e46c282cbec489ac3f10b4d6
steve <stev8855@gmail.com>
2024-05-19 20:14:06 -0500

    feat: add CORS middleware to app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import { corsMiddleware } from "./src/middlewares/cors";
 
 dotenv.config();
 const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
+app.use(corsMiddleware());
 
 const PORT = process.env.PORT || 3000;
 
