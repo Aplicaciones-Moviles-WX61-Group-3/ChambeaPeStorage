3f461ef07c1b3d44f1e600adcf808c679e104e85
steve <stev8855@gmail.com>
2024-05-20 13:12:19 -0500

    feat: add controller for mongo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,4 +11,49 @@ export class UserController {
             res.status(500).json({ error: error.message });
         }
     }
+
+    getUserById = async (req, res) => {
+        try {
+            const user = await this.userService.getUserById(req.params.id_user);
+            res.json(user);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    createUser = async (req, res) => {
+        try {
+            const user = await this.userService.createUser(req.body);
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    deleteUser = async (req, res) => {
+        try {
+            await this.userService.deleteUser(req.params.id_user);
+            res.status(204).json();
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    addImageUrl = async (req, res) => {
+        try {
+            const user = await this.userService.addImageUrl(req.params.id_user, req.body.imageUrl);
+            res.json(user);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    removeImageUrl = async (req, res) => {
+        try {
+            const user = await this.userService.removeImageUrl(req.params.id_user, req.body.imageUrl);
+            res.json(user);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
 }
