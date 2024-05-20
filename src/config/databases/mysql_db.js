809ea686cf134d0e06188e291b0200a502779a31
steve <stev8855@gmail.com>
2024-05-19 20:07:30 -0500

    feat: add user service to test connection

--- src/config/databases/mysql_db.js
+++ src/config/databases/mysql_db.js
@@ -4,8 +4,4 @@ import database from './keys';
 
 const pool = mysql.createPool(database.database);
 
-pool.getConnection((err, conn) => {
-    console.log('Connected to the database');
-});
-
 export default pool;
